fc914a891ded7e4511572676cf3b6c754b8c68db
Aman Kumar Singh Kushwaha <96011814+Aman-Singh-Kushwaha@users.noreply.github.com>
2023-12-12 01:05:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
-from flask import Flask, request, render_template
-# from google.cloud import storage
+from flask import Flask, render_template
 
 app = Flask(__name__)
-# storage_client = storage.Client()
 
 @app.route('/')
-def index():
-    return 'Alright It worked'
-
-# @app.route('/upload', methods=['POST'])
-# def upload():
-#     uploaded_files = request.files.getlist('file')
-#     bucket_name = 'your-gcs-bucket-name'
-
-#     for file in uploaded_files:
-#         blob = storage_client.bucket(bucket_name).blob(file.filename)
-#         blob.upload_from_file(file.stream)
-
-#     return 'File(s) uploaded successfully to GCS'
+def home_func():
+    return render_template('home.html');
 
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
